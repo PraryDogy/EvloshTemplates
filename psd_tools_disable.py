42210c90c974aa484029e18e8b88c04ea4e96a11
Morkik <evlosh@gmail.com>
2024-10-05 15:54:47 +0300

    reff

---
+++ psd_tools_disable.py
@@ -0,0 +1,6 @@
+import psd_tools
+import logging
+
+psd_tools.psd.tagged_blocks.warn = lambda *args, **kwargs: None
+psd_logger = logging.getLogger("psd_tools")
+psd_logger.setLevel(logging.CRITICAL)
