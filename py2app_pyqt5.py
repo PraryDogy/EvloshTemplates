e6e8d511d9301252785d540e60fda2e61237a8e4
Pupitor 9000 <evlosh@gmail.com>
2025-07-20 22:10:08 +0300

    ref

--- py2app_pyqt5.py
+++ py2app_pyqt5.py
@@ -1,29 +1,11 @@
+"""
+Python 3.11
+"""
+
 import os
-import subprocess
-import sys
-import traceback
 
 
-class System_:
-
-    @classmethod
-    def catch_error(cls, *args) -> None:
-
-        STARS = "*" * 40
-        ABOUT = "Отправьте это сообщение в telegram @evlosh или на почту loshkarev@miuz.ru"
-        ERROR = traceback.format_exception(*args)
-
-        SUMMARY_MSG = "\n".join([*ERROR, STARS, ABOUT])
-        
-        # 
-        # 
-        # 
-        # скопируй к себе scpt файл
-        script = "error_msg.scpt"
-        subprocess.run(["osascript", script, SUMMARY_MSG])
-        # 
-        # 
-        # 
+class PyQt5App:
 
     @classmethod
     def set_plugin_path(cls) -> bool:
@@ -34,11 +16,6 @@ class System_:
             return True
         else:
             return False
-        
-    @classmethod
-    def set_excepthook(cls) -> None:
-        sys.excepthook = cls.catch_error
 
 
-if System_.set_plugin_path():
-    System_.set_excepthook()
+PyQt5App.set_plugin_path()
