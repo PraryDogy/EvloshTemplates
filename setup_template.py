3403eb9a4915ed962fbcfe34b70ad104a41a3ebf
Pupitor 9000 <evlosh@gmail.com>
2025-07-20 22:08:17 +0300

    куа

--- setup_template.py
+++ setup_template.py
@@ -86,15 +86,21 @@ YEAR = datetime.now().year # CURRENT YEAR
 BUNDLE_ID = f"com.{SHORT_AUTHOR_NAME}.{APP_NAME}" # DON'T CHANGE IT
 PY2APP = "py2app" # DON'T CHANGE IT
 
-OPTIONS = {"iconfile": ICON_PATH,
-           "plist": {"CFBundleName": APP_NAME,
-                     "CFBundleShortVersionString": APP_VER,
-                     "CFBundleVersion": APP_VER,
-                     "CFBundleIdentifier": BUNDLE_ID,
-                     "NSHumanReadableCopyright": (
-                         f"Created by {AUTHOR}"
-                         f"\nCopyright © {YEAR} {COMPANY}."
-                         f"\nAll rights reserved.")}}
+OPTIONS = {
+    "excludes": ["setuptools"],
+    "iconfile": ICON_PATH,
+        "plist": {
+            "CFBundleName": APP_NAME,
+            "CFBundleShortVersionString": APP_VER,
+            "CFBundleVersion": APP_VER,
+            "CFBundleIdentifier": BUNDLE_ID,
+            "NSHumanReadableCopyright": (
+                f"Created by {AUTHOR}"
+                f"\nCopyright © {YEAR} {COMPANY}."
+                f"\nAll rights reserved."
+            )
+        }
+}
 
 sys.argv.append(PY2APP)
 
